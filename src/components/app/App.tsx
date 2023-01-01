3757520217e60b02433047b6ea8e4263af1358e4
KonstantinKhan <khan366kos@gmail.com>
2023-01-01 12:21:48 +0500

    init project.

---
+++ src/components/app/App.tsx
@@ -0,0 +1,17 @@
+import React, {FC} from 'react';
+import {Button} from "primereact/button";
+
+import "./app.sass"
+
+const App: FC = () => {
+
+    console.log("render AppComponent")
+
+    return (
+        <div className={"App"}>
+            <Button label="click-click"/>
+        </div>
+    )
+}
+
+export default App;
